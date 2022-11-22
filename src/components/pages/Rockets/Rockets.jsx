7556f914dd86e9f337d1ebb85289c54a1dc54119
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-11-22 18:34:41 +0500

    Fix linter Errors

--- src/components/pages/Rockets/Rockets.jsx
+++ src/components/pages/Rockets/Rockets.jsx
@@ -1,39 +1,37 @@
 /* eslint linebreak-style: ["error", "windows"] */
-/* eslint-disable */
 
 import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRockets } from '../../../redux/rockets/rockets';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
-import "./Rocket.css";
 import { v4 as uuidv4 } from 'uuid';
+import {
+  Container, Row, Col, Image, Button,
+} from 'react-bootstrap';
+import { fetchRockets } from '../../../redux/rockets/rockets';
+import './Rocket.css';
 
 const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rocketsReducer);
   const dataFetchedRef = useRef(false);
   useEffect(() => {
-    if (dataFetchedRef.current){ return }
-    else{
+    if (dataFetchedRef.current);
+    else {
       dataFetchedRef.current = true;
       dispatch(fetchRockets());
-  }
-}, [rockets])
+    }
+  }, [rockets]);
   return (
     <Container fluid className="container">
-      {rockets.map(element => {
-      return( 
-      <Row key={uuidv4()}>
-        <Col className="eachbox" xs={5}><Image fluid src={element.flickr_images[0]} alt= {element.flickr_images[0]}/></Col>
-        <Col className="eachbox">
-          <h1>{element.name}</h1>
-          <p>{element.description}</p>
-          <Button variant="primary" type="button">Reserve Rocket</Button>
-        </Col>
-      </Row>
-      )
-      })}
-      
+      {rockets.map((element) => (
+        <Row key={uuidv4()}>
+          <Col className="eachbox" xs={5}><Image fluid src={element.flickr_images[0]} alt={element.flickr_images[0]} /></Col>
+          <Col className="eachbox">
+            <h1>{element.name}</h1>
+            <p>{element.description}</p>
+            <Button variant="primary" type="button">Reserve Rocket</Button>
+          </Col>
+        </Row>
+      ))}
     </Container>
   );
 };
